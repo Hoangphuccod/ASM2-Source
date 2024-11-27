765404e2b8b44900fa91efb4cef61b973cc3104b
Hoangphuccod <hoangphuc36115@gmail.com>
2024-11-27 09:31:16 +0700

    Add files via upload

---
+++ WaterBillManagement/WaterBillManagement/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WaterBillManagement
+{
+    partial class row
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtctn = new System.Windows.Forms.TextBox();
+            this.txtwlm = new System.Windows.Forms.TextBox();
+            this.txtwtm = new System.Windows.Forms.TextBox();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.cbtype = new System.Windows.Forms.ComboBox();
+            this.Export = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(500, 172);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 467);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtctn
+            // 
+            this.txtctn.Location = new System.Drawing.Point(266, 203);
+            this.txtctn.Name = "txtctn";
+            this.txtctn.Size = new System.Drawing.Size(100, 26);
+            this.txtctn.TabIndex = 2;
+            // 
+            // txtwlm
+            // 
+            this.txtwlm.Location = new System.Drawing.Point(266, 261);
+            this.txtwlm.Name = "txtwlm";
+            this.txtwlm.Size = new System.Drawing.Size(100, 26);
+            this.txtwlm.TabIndex = 4;
+            // 
+            // txtwtm
+            // 
+            this.txtwtm.Location = new System.Drawing.Point(266, 335);
+            this.txtwtm.Name = "txtwtm";
+            this.txtwtm.Size = new System.Drawing.Size(100, 26);
+            this.txtwtm.TabIndex = 6;
+            // 
+            // txtnum
+            // 
+            this.txtnum.Location = new System.Drawing.Point(266, 495);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(100, 26);
+            this.txtnum.TabIndex = 9;
+            // 
+            // cbtype
+            // 
+            this.cbtype.FormattingEnabled = true;
+            this.cbtype.Items.AddRange(new object[] {
+            "Household",
+            "Business",
+            "ManuaFactory",
+            "Government"});
+            this.cbtype.Location = new System.Drawing.Point(256, 420);
+            this.cbtype.Name = "cbtype";
+            this.cbtype.Size = new System.Drawing.Size(121, 28);
+            this.cbtype.TabIndex = 10;
+            this.cbtype.SelectedIndexChanged += new System.EventHandler(this.cbtype_SelectedIndexChanged);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(26, 604);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(75, 36);
+            this.Export.TabIndex = 11;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(131, 603);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 36);
+            this.save.TabIndex = 12;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(247, 604);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Sort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(722, 111);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(202, 26);
+            this.textBox11.TabIndex = 18;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Customer\'s Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Last Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "This Month";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 423);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(114, 501);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Number of People";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(631, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Search";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(961, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 604);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 35);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // row
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1339, 765);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.cbtype);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.txtwtm);
+            this.Controls.Add(this.txtwlm);
+            this.Controls.Add(this.txtctn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "row";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtctn;
+        private System.Windows.Forms.TextBox txtwlm;
+        private System.Windows.Forms.TextBox txtwtm;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.ComboBox cbtype;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WaterBillManagement/WaterBillManagement/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WaterBillManagement
+{
+    public partial class row : Form
+    {
+        string[] names = new string[100];
+        int[] types = new int[100];
+        double[] lastMonths = new double[100];
+        double[] thisMonths = new double[100];
+        int[] numberPeoples = new int[100];
+        double[] usages = new double[100];
+        double[] totalCosts = new double[100];
+
+        int n = 0;
+
+        public row()
+        {
+            InitializeComponent();
+        }
+        private void Display()
+        {
+
+            dataGridView1.DataSource = null;
+            var bills = new DataTable(); 
+            bills.Columns.Add("Name"); 
+            bills.Columns.Add("Last Month"); 
+            bills.Columns.Add("This Month"); 
+            bills.Columns.Add("Usage");
+            bills.Columns.Add("Total Cost");
+            for (int i = 0; i < n; i++)
+            {
+                bills.Rows.Add(names[i], lastMonths[i], thisMonths[i], usages[i], totalCosts[i]);
+            }
+            dataGridView1.DataSource = bills;
+            dataGridView1.Refresh();
+        }
+        
+
+        public void ClearData()
+        {
+            txtctn.Text = "";
+            txtwlm.Text = "";
+            txtwtm.Text = "";
+            txtnum.Text = "";
+            cbtype.Text = "";
+            Display();
+        }
+        private void cbCustomerType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private double GetPriceUnit(int customerType, double usageTotal)
+        {
+            double priceUnit = 0;
+
+            if (customerType == 0)
+            {
+                int numberPeople = int.Parse(txtnum.Text);
+                double avg = usageTotal / numberPeople;
+
+                if (avg <= 10)
+                {
+                    priceUnit = 5973;
+                }
+                else if (avg <= 20)
+                {
+                    priceUnit = 7052;
+                }
+                else if (avg <= 30)
+                {
+                    priceUnit = 8699;
+                }
+                else
+                {
+                    priceUnit = 15929;
+                }
+            }
+            else if (customerType == 1)
+            {
+                priceUnit = 9955;
+            }
+            else if (customerType == 2)
+            {
+                priceUnit = 11615;
+            }
+            else if (customerType == 3)
+            {
+                priceUnit = 22068;
+            }
+
+            return priceUnit;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            string customerName = txtctn.Text;
+            double waterMeterLastMonth = double.Parse(txtwlm.Text);
+            double waterMeterThisMonth = double.Parse(txtwtm.Text);
+            int customerType = cbtype.SelectedIndex;
+            double usageTotal = waterMeterThisMonth - waterMeterLastMonth;
+            double priceUnit = GetPriceUnit(customerType, usageTotal);
+            double totalBill = priceUnit * usageTotal * 1.1;
+
+            MessageBox.Show("Customer name: " + customerName + "\n" +
+                            "Usage: " + usageTotal + " m3" + "\n" +
+                            "Bill: " + totalBill + " VND");
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string customerName = txtctn.Text;
+            double waterMeterLastMonth = double.Parse(txtwlm.Text);
+            double waterMeterThisMonth = double.Parse(txtwtm.Text);
+            int customerType = cbtype.SelectedIndex;
+            double usageTotal = waterMeterThisMonth - waterMeterLastMonth;
+            double priceUnit = GetPriceUnit(customerType, usageTotal);
+            double totalBill = priceUnit * usageTotal * 1.1;
+
+            names[n] = customerName;
+            types[n] = customerType;
+            lastMonths[n] = waterMeterLastMonth;
+            thisMonths[n] = waterMeterThisMonth;
+            numberPeoples[n] = (customerType == 0) ? int.Parse(txtnum.Text) : 0;
+            usages[n] = usageTotal;
+            totalCosts[n] = totalBill;
+            n++;
+
+            Display();
+            MessageBox.Show("Saved successfully");
+            ClearData();
+        }
+
+
+        private void dgvBills_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            txtctn.Text = names[index];
+            cbtype.SelectedIndex = types[index];
+            txtwlm.Text = lastMonths[index].ToString();
+            txtwtm.Text = thisMonths[index].ToString();
+            txtnum.Text = (types[index] == 0) ? numberPeoples[index].ToString() : "";
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbtype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = cbtype.SelectedIndex;
+
+            if (selectedIndex == 0)
+            {
+                label5.Visible = true;
+                txtnum.Visible = true;
+            }
+            else
+            {
+                label5.Visible = false;
+                txtnum.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string searchTerm = textBox11.Text.Trim();
+
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                MessageBox.Show("Please enter the name of a customer.");
+                return;
+            }
+
+            var searchResults = new DataTable();
+            searchResults.Columns.Add("Name");
+            searchResults.Columns.Add("Last Month");
+            searchResults.Columns.Add("This Month");
+            searchResults.Columns.Add("Usage");
+            searchResults.Columns.Add("Total Cost");
+
+            for (int i = 0; i < n; i++)
+            {
+                if (names[i].IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    searchResults.Rows.Add(names[i], lastMonths[i], thisMonths[i], usages[i], totalCosts[i]);
+                }
+            }
+
+            if (searchResults.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = searchResults;
+            }
+            else
+            {
+                MessageBox.Show("No matching customer found.");
+                dataGridView1.DataSource = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var customerData = new List<(string Name, int Type, double LastMonth, double ThisMonth, int NumberPeople, double Usage, double TotalCost)>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customerData.Add((names[i], types[i], lastMonths[i], thisMonths[i], numberPeoples[i], usages[i], totalCosts[i]));
+            }
+
+            customerData = customerData.OrderBy(data => data.TotalCost).ToList();
+
+            for (int i = 0; i < customerData.Count; i++)
+            {
+                names[i] = customerData[i].Name;
+                types[i] = customerData[i].Type;
+                lastMonths[i] = customerData[i].LastMonth;
+                thisMonths[i] = customerData[i].ThisMonth;
+                numberPeoples[i] = customerData[i].NumberPeople;
+                usages[i] = customerData[i].Usage;
+                totalCosts[i] = customerData[i].TotalCost;
+            }
+
+            Display();
+            MessageBox.Show("Sorted by total cost in ascending order.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+
+                for (int i = selectedIndex; i < n - 1; i++)
+                {
+                    names[i] = names[i + 1];
+                    types[i] = types[i + 1];
+                    lastMonths[i] = lastMonths[i + 1];
+                    thisMonths[i] = thisMonths[i + 1];
+                    numberPeoples[i] = numberPeoples[i + 1];
+                    usages[i] = usages[i + 1];
+                    totalCosts[i] = totalCosts[i + 1];
+                }
+
+                n--;
+                Display();
+                MessageBox.Show("Row deleted successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+    }
+}
